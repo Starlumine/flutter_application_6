e31205a59b5750e87f5d208dea9bb8bc6e7fc6f7
Starlumine <jchan1star@gmail.com>
2025-03-09 15:08:43 -0500

    Added second and third floor questions
    More questions added. Counter works across all pages now.

--- flutter_application_6/lib/main.dart
+++ flutter_application_6/lib/main.dart
@@ -9,11 +9,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const TitlePage(),
+    return ProgressTracker(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: const TitlePage(),
+      ),
     );
   }
+}
+
+// Tracks and stores the total score
+class ProgressTracker extends InheritedWidget {
+  final int totalQuestions = 11;
+  final ValueNotifier<int> scoreNotifier = ValueNotifier<int>(0);
+  final List<bool> answeredQuestions = List.generate(11, (_) => false); // List to track all answers
+
+  ProgressTracker({super.key, required super.child});
+
+  static ProgressTracker of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<ProgressTracker>()!;
+  }
+
+  @override
+  bool updateShouldNotify(covariant ProgressTracker oldWidget) => true;
 }
 
 class TitlePage extends StatelessWidget {
@@ -28,10 +46,7 @@ class TitlePage extends StatelessWidget {
           children: [
             const Text(
               "Scavenger Hunt of PFT",
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 20),
@@ -39,13 +54,10 @@ class TitlePage extends StatelessWidget {
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const ScavengerHuntPage()),
+                  MaterialPageRoute(builder: (context) => const FirstFloorPage()),
                 );
               },
-              child: const Text(
-                "START",
-                style: TextStyle(fontSize: 18),
-              ),
+              child: const Text("START", style: TextStyle(fontSize: 18)),
             ),
           ],
         ),
@@ -54,50 +66,22 @@ class TitlePage extends StatelessWidget {
   }
 }
 
-class ScavengerHuntPage extends StatefulWidget {
-  const ScavengerHuntPage({super.key});
+class FirstFloorPage extends StatefulWidget {
+  const FirstFloorPage({super.key});
 
   @override
-  _ScavengerHuntPageState createState() => _ScavengerHuntPageState();
+  FirstFloorPageState createState() => FirstFloorPageState();
 }
 
-class _ScavengerHuntPageState extends State<ScavengerHuntPage> {
-  int score = 0;
-  final int totalQuestions = 14;
-  
-  // List of questions and correct answers
-  final List<Map<String, String>> questions = [
-    {"question": "What is the maximum occupancy of Panera Bread?", "answer": "88"},
-    {"question": "What is written on the outlet underneath the room 1100 sign? (Type without spaces)", "answer": "6P1B4"},
-    {"question": "Located near room 1126. How many pounds does the AgBot 2G weigh? (Enter a whole number)", "answer": "25"},
-    {"question": "What snack is on number 160 at the vending machine?", "answer": "Poptarts"},
-    {"question": "How many outlets are on the large stairs next to the Chevron Center?", "answer": "14"},
-    {"question": "On the purple banners right above the large stairs, what is the word that starts with the letter T on the banner?", "answer": "Teammate"},
+class FirstFloorPageState extends State<FirstFloorPage> {
+  final List<Map<String, dynamic>> questions = [
+    {"question": "What is the maximum occupancy of Panera Bread?", "answer": "88", "answered": false},
+    {"question": "What is written on the outlet underneath the room 1100 sign? (Type without spaces)", "answer": "6P1B4", "answered": false},
+    {"question": "Located near room 1126. How many pounds does the AgBot 2G weigh?", "answer": "25", "answered": false},
+    {"question": "What snack is on number 160 at the vending machine?", "answer": "Poptarts", "answered": false},
+    {"question": "How many outlets are on the large stairs next to the Chevron Center?", "answer": "14", "answered": false},
+    {"question": "On the purple banners right above the large stairs, what is the word that starts with the letter T on the banner?", "answer": "Teammate", "answered": false},
   ];
-
-  final Map<int, TextEditingController> controllers = {};
-  final Map<int, bool> correctAnswers = {};
-
-  @override
-  void initState() {
-    super.initState();
-    for (int i = 0; i < questions.length; i++) {
-      controllers[i] = TextEditingController();
-      correctAnswers[i] = false;
-    }
-  }
-
-  void checkAnswer(int index) {
-    String userAnswer = controllers[index]!.text.trim();
-    String correctAnswer = questions[index]["answer"]!.trim();
-
-    if (userAnswer.toLowerCase() == correctAnswer.toLowerCase() && !correctAnswers[index]!) {
-      setState(() {
-        correctAnswers[index] = true;
-        score++;
-      });
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -107,49 +91,243 @@ class _ScavengerHuntPageState extends State<ScavengerHuntPage> {
         actions: [
           Padding(
             padding: const EdgeInsets.all(12.0),
-            child: Center(
-              child: Text(
-                "$score / $totalQuestions",
-                style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
+            child: ValueListenableBuilder<int>(
+              valueListenable: ProgressTracker.of(context).scoreNotifier,
+              builder: (context, score, child) {
+                return Text("$score / ${ProgressTracker.of(context).totalQuestions}");
+              },
             ),
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: questions.length,
-          itemBuilder: (context, index) {
-            return Card(
-              elevation: 3,
-              margin: const EdgeInsets.symmetric(vertical: 8),
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      questions[index]["question"]!,
-                      style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 8),
-                    TextField(
-                      controller: controllers[index],
-                      onSubmitted: (_) => checkAnswer(index),
-                      decoration: InputDecoration(
-                        hintText: "Enter your answer",
-                        suffixIcon: correctAnswers[index] == true
-                            ? const Icon(Icons.check_circle, color: Colors.green)
-                            : null,
-                        border: const OutlineInputBorder(),
-                      ),
-                    ),
-                  ],
+      body: _buildFirstFloorQuestionList(context),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SecondFloorPage()),
+          );
+        },
+        label: const Text("Next: Second Floor"),
+        icon: const Icon(Icons.arrow_forward),
+      ),
+    );
+  }
+
+  Widget _buildFirstFloorQuestionList(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemCount: questions.length,
+      itemBuilder: (context, index) {
+        return _buildQuestionCard(context, questions[index], index);
+      },
+    );
+  }
+
+  Widget _buildQuestionCard(BuildContext context, Map<String, dynamic> question, int index) {
+    TextEditingController controller = TextEditingController();
+    final progressTracker = ProgressTracker.of(context);
+
+    return Card(
+      elevation: 3,
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      child: ListTile(
+        title: Text(question["question"]!, style: const TextStyle(fontWeight: FontWeight.bold)),
+        subtitle: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: controller,
+                decoration: InputDecoration(
+                  hintText: "Enter your answer",
+                  enabled: !progressTracker.answeredQuestions[index], // Disable if answered
                 ),
+                onSubmitted: (input) {
+                  if (input.trim().toLowerCase() == question["answer"]!.toLowerCase() && !progressTracker.answeredQuestions[index]) {
+                    setState(() {
+                      progressTracker.answeredQuestions[index] = true; // Mark as answered
+                      progressTracker.scoreNotifier.value++; // Increment score if correct
+                    });
+                  }
+                },
               ),
-            );
-          },
+            ),
+            if (progressTracker.answeredQuestions[index])
+              const Icon(Icons.check_circle, color: Colors.green), // Display checkmark if answered correctly
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SecondFloorPage extends StatefulWidget {
+  const SecondFloorPage({super.key});
+
+  @override
+  SecondFloorPageState createState() => SecondFloorPageState();
+}
+
+class SecondFloorPageState extends State<SecondFloorPage> {
+  final List<Map<String, dynamic>> questions = [
+    {"question": "What is the company name on the top right of every thermostat in the building?", "answer": "Johnson Controls", "answered": false},
+    {"question": "The student services office is named after a person. What is their last name?", "answer": "Brookshire", "answered": false},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("PFT Second Floor Questions"),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: ValueListenableBuilder<int>(
+              valueListenable: ProgressTracker.of(context).scoreNotifier,
+              builder: (context, score, child) {
+                return Text("$score / ${ProgressTracker.of(context).totalQuestions}");
+              },
+            ),
+          ),
+        ],
+      ),
+      body: _buildSecondFloorQuestionList(context),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => ThirdFloorPage()),
+          );
+        },
+        label: const Text("Next: Third Floor"),
+        icon: const Icon(Icons.arrow_forward),
+      ),
+    );
+  }
+
+  Widget _buildSecondFloorQuestionList(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemCount: questions.length,
+      itemBuilder: (context, index) {
+        return _buildQuestionCard(context, questions[index], index + 6); // Adjust index for second floor
+      },
+    );
+  }
+
+  Widget _buildQuestionCard(BuildContext context, Map<String, dynamic> question, int index) {
+    TextEditingController controller = TextEditingController();
+    final progressTracker = ProgressTracker.of(context);
+
+    return Card(
+      elevation: 3,
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      child: ListTile(
+        title: Text(question["question"]!, style: const TextStyle(fontWeight: FontWeight.bold)),
+        subtitle: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: controller,
+                decoration: InputDecoration(
+                  hintText: "Enter your answer",
+                  enabled: !progressTracker.answeredQuestions[index], // Disable if answered
+                ),
+                onSubmitted: (input) {
+                  if (input.trim().toLowerCase() == question["answer"]!.toLowerCase() && !progressTracker.answeredQuestions[index]) {
+                    setState(() {
+                      progressTracker.answeredQuestions[index] = true; // Mark as answered
+                      progressTracker.scoreNotifier.value++; // Increment score if correct
+                    });
+                  }
+                },
+              ),
+            ),
+            if (progressTracker.answeredQuestions[index])
+              const Icon(Icons.check_circle, color: Colors.green), // Display checkmark if answered correctly
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ThirdFloorPage extends StatefulWidget {
+  const ThirdFloorPage({super.key});
+
+  @override
+  ThirdFloorPageState createState() => ThirdFloorPageState();
+}
+
+class ThirdFloorPageState extends State<ThirdFloorPage> {
+  final List<Map<String, dynamic>> questions = [
+    {"question": "On the third floor, there is an area you can walk outside of. What is this area called?", "answer": "West Terrace", "answered": false},
+    {"question": "What room number is David Shepherd's room?", "answer": "3272W", "answered": false},
+    {"question": "Next to room 3207, there are two checks in a display case. What number is on the check for the PetroBowl XVIII?", "answer": "5000", "answered": false},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("PFT Third Floor Questions"),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: ValueListenableBuilder<int>(
+              valueListenable: ProgressTracker.of(context).scoreNotifier,
+              builder: (context, score, child) {
+                return Text("$score / ${ProgressTracker.of(context).totalQuestions}");
+              },
+            ),
+          ),
+        ],
+      ),
+      body: _buildThirdFloorQuestionList(context),
+    );
+  }
+
+  Widget _buildThirdFloorQuestionList(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemCount: questions.length,
+      itemBuilder: (context, index) {
+        return _buildQuestionCard(context, questions[index], index + 8); // Adjust index for third floor
+      },
+    );
+  }
+
+  Widget _buildQuestionCard(BuildContext context, Map<String, dynamic> question, int index) {
+    TextEditingController controller = TextEditingController();
+    final progressTracker = ProgressTracker.of(context);
+
+    return Card(
+      elevation: 3,
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      child: ListTile(
+        title: Text(question["question"]!, style: const TextStyle(fontWeight: FontWeight.bold)),
+        subtitle: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: controller,
+                decoration: InputDecoration(
+                  hintText: "Enter your answer",
+                  enabled: !progressTracker.answeredQuestions[index], // Disable if answered
+                ),
+                onSubmitted: (input) {
+                  if (input.trim().toLowerCase() == question["answer"]!.toLowerCase() && !progressTracker.answeredQuestions[index]) {
+                    setState(() {
+                      progressTracker.answeredQuestions[index] = true; // Mark as answered
+                      progressTracker.scoreNotifier.value++; // Increment score if correct
+                    });
+                  }
+                },
+              ),
+            ),
+            if (progressTracker.answeredQuestions[index])
+              const Icon(Icons.check_circle, color: Colors.green), // Display checkmark if answered correctly
+          ],
         ),
       ),
     );
